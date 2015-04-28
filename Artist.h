4a0f3600c521103fc68e983daa526280aaa61dd7
fdesimini <fdesimini@users.noreply.github.com>
2015-04-27 22:05:45 -0400

    Adding an artist class - to create a custom tableview cell

---
+++ Artist.h
@@ -0,0 +1,19 @@
+//
+//  Artist.h
+//  MercarttoSimpleVersion
+//
+//  Created by Frank Desimini on 2015-04-27.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Artist : NSObject
+
+@property (strong, nonatomic) NSString *artTitle;
+@property (strong, nonatomic) NSString *artistName;
+@property (strong, nonatomic) NSString *artImage;
+@property (strong, nonatomic) NSString *artLocation;
+//@property (strong, nonatomic) NSArray *artDetails;
+
+@end
---
+++ Artist.m
@@ -0,0 +1,13 @@
+//
+//  Artist.m
+//  MercarttoSimpleVersion
+//
+//  Created by Frank Desimini on 2015-04-27.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+@end
